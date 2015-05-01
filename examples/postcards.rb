be027db5d1d3bd96cca825b7a1cf7eb3cf0d9c77
Marcus Gartner <marcus@lob.com>
2015-05-01 12:18:10 -0700

    updated postcard example

--- examples/postcards.rb
+++ examples/postcards.rb
@@ -5,6 +5,56 @@ require 'lob'
 Lob.api_key = 'test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc'
 @lob = Lob.load
 
+# HTML to send to the server
+html = %{
+<html>
+<head>
+<title>Lob.com Sample 4x6 Postcard Front</title>
+<style>
+  *, *:before, *:after {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: 'Loved by the King';
+    font-style: normal;
+    font-weight: 400;
+    src: url('https://s3-us-west-2.amazonaws.com/lob-assets/LovedbytheKing.ttf') format('truetype');
+  }
+
+  body {
+    width: 6.25in;
+    height: 4.25in;
+    margin: 0;
+    padding: 0;
+    /* If using an image, the background image should have dimensions of 1875x1275 pixels. */
+    background-image: url(https://s3-us-west-2.amazonaws.com/lob-assets/beach.jpg);
+    background-size: 6.25in 4.25in;
+    background-repeat: no-repeat;
+  }
+
+  .text {
+    margin: 50px;
+    width: 400px;
+    font-family: 'Loved by the King';
+    font-size: 50px;
+    font-weight: 700;
+    color: white;
+    text-shadow: 3px 3px black;
+  }
+</style>
+</head>
+
+<body>
+  <h1 class="text">Hello {{name}}!</h1>
+  <p class="text">Join us for the {{event}}!</p>
+</body>
+
+</html>
+}
+
 # create a to address
 to_address = @lob.addresses.create(
   name: "ToAddress",
@@ -27,9 +77,11 @@ from_address = @lob.addresses.create(
 
 # send a postcard
 puts @lob.postcards.create(
-  name: "John Joe",
+  description: "Beach Postcard",
   to: to_address["id"],
   from: from_address["id"],
-  front: "https://s3-us-west-2.amazonaws.com/lob-assets/postcardfront.pdf",
-  back: "https://s3-us-west-2.amazonaws.com/lob-assets/postcardback.pdf"
+  metadata: { campaign: "Summer 2015 Beach" },
+  data: { name: "Albert", event: "Summer 2015 Beach-athon" },
+  front: html,
+  message: "Please RSVP as soon as possible to reserve your lounge chair."
 )
