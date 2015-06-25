7fa18b07eace9848ca82daaa99d3d3d3288724b3
Rafa de Castro <rafael@micubiculo.com>
2015-06-26 00:11:20 +0200

    This seems like a better idea to log an error when an exception occurs. The lob error class should not override to_s or the already existing in StandardError method "message". Instead of that we just rely on the behaviour of the default exception classes.

--- lib/lob/errors/lob_error.rb
+++ lib/lob/errors/lob_error.rb
@@ -1,17 +1,17 @@
 module Lob
   class LobError < StandardError
-    attr_reader :message, :http_status, :http_body, :json_body
+    attr_reader :http_status, :http_body, :json_body
 
-    def initialize(message=nil, http_status=nil, http_body=nil, json_body=nil)
-      @message = message
+    def initialize(message, http_status=nil, http_body=nil, json_body=nil)
       @http_status = http_status
       @http_body = http_body
       @json_body = json_body
+      super(custom_message)
     end
 
-    def to_s
+    def custom_message
       status_string = @http_status.nil? ? "" : "(Status #{@http_status}) "
-      "#{status_string}#{@message}"
+      "#{status_string}#{to_s} \n #{@http_body}"
     end
   end
 end
