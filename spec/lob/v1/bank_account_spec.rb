0a1ff3f65a4c25cd4830664d99569fee8e58a4b0
Marcus Gartner <marcus@lob.com>
2015-11-06 13:25:34 -0800

    feat(upgrade): update all test and examples to work with 2015-11-06

--- spec/lob/v1/bank_account_spec.rb
+++ spec/lob/v1/bank_account_spec.rb
@@ -3,18 +3,6 @@ require "spec_helper"
 describe Lob::V1::BankAccount do
 
   before :each do
-
-    @sample_address_params = {
-      name:    "TestAddress",
-      email:   "test@test.com",
-      address_line1: "123 Test Street",
-      address_line2: "Unit 199",
-      address_city:    "Mountain View",
-      address_state:   "CA",
-      address_country: "US",
-      address_zip:     94085
-    }
-
     @sample_bank_account_params = {
       routing_number: "122100024",
       account_number: "123456789"
@@ -31,44 +19,25 @@ describe Lob::V1::BankAccount do
 
 
   describe "create" do
-    it "should create a bank account with address_id" do
-      new_address = subject.addresses.create @sample_address_params
 
+    it "should create a bank account" do
       result = subject.bank_accounts.create(
         routing_number: @sample_bank_account_params[:routing_number],
-        bank_address: new_address["id"],
         account_number: @sample_bank_account_params[:account_number],
-        account_address: new_address["id"],
         signatory: "John Doe"
       )
 
       result["account_number"].must_equal(@sample_bank_account_params[:account_number])
     end
 
-
-    it "should create a bank account with address params" do
-      result = subject.bank_accounts.create(
-        routing_number: @sample_bank_account_params[:routing_number],
-        bank_address: @sample_address_params.clone,
-        account_number: @sample_bank_account_params[:account_number],
-        account_address: @sample_address_params.clone,
-        signatory: "John Doe"
-      )
-
-      result["account_number"].must_equal(@sample_bank_account_params[:account_number])
-    end
   end
 
 
   describe "find" do
     it "should find a bank account" do
-      new_address = subject.addresses.create @sample_address_params
-
       new_bank_account = subject.bank_accounts.create(
         routing_number: @sample_bank_account_params[:routing_number],
-        bank_address: new_address["id"],
         account_number: @sample_bank_account_params[:account_number],
-        account_address: new_address["id"],
         signatory: "John Doe"
       )
 
@@ -81,9 +50,7 @@ describe Lob::V1::BankAccount do
     it "should delete a bank_account" do
       new_bank_account = subject.bank_accounts.create(
         routing_number: @sample_bank_account_params[:routing_number],
-        bank_address: @sample_address_params.clone,
         account_number: @sample_bank_account_params[:account_number],
-        account_address: @sample_address_params.clone,
         signatory: "John Doe"
       )
 
@@ -96,9 +63,7 @@ describe Lob::V1::BankAccount do
     it "should verify a bank account" do
       new_bank_account = subject.bank_accounts.create(
         routing_number: @sample_bank_account_params[:routing_number],
-        bank_address: @sample_address_params.clone,
         account_number: @sample_bank_account_params[:account_number],
-        account_address: @sample_address_params.clone,
         signatory: "John Doe"
       )
 
