628cfc50da93be9754d485a4d485a6cc0fbe3fd9
Matt Piercy <mpiercy@gmail.com>
2017-05-30 11:51:45 -0700

    chore(examples): Update examples to use new us_verifications endpoint

--- examples/verify/verify.rb
+++ examples/verify/verify.rb
@@ -3,33 +3,32 @@ $:.unshift File.expand_path("../../lib", File.dirname(__FILE__))
 require 'csv'
 require 'lob'
 
-# Be sure to install the StreetAddress gem.
-require 'street_address'
-
 # Initialize Lob object
 lob = Lob::Client.new(api_key: 'test_799ff27291c166d10ba191902ad02fb059c')
 
 output = File.open(File.expand_path('../output.csv', __FILE__), 'w')
 
-output.puts ['address_line1', 'address_city', 'address_state', 'address_zip', 'address_country'].join(',')
+output.puts ['primary_line', 'secondary_line', 'urbanization', 'last_line', 'deliverability'].join(',')
 
 # Parse the input file and verify the addresses
-File.open(File.expand_path('../input.txt', __FILE__)).each_line do |line|
-  parsed_address = StreetAddress::US.parse(line)
+File.open(File.expand_path('../input.csv', __FILE__)).each_line do |line|
+  address_components = line.split(',')
 
-  verified_address = lob.addresses.verify(
-    address_line1: parsed_address.to_s(:line1),
-    address_city: parsed_address.city,
-    address_state: parsed_address.state,
-    address_country: 'US'
-  )['address']
+  verified_address = lob.us_verifications.verify(
+    primary_line: address_components[0],
+    secondary_line: address_components[1],
+    urbanization: address_components[2],
+    city: address_components[3],
+    state: address_components[4],
+    zip_code: address_components[5]
+  )
 
   output.puts [
-    verified_address['address_line1'],
-    verified_address['address_city'],
-    verified_address['address_state'],
-    verified_address['address_zip'],
-    verified_address['address_country']
+    verified_address['primary_line'],
+    verified_address['secondary_line'],
+    verified_address['urbanization'],
+    verified_address['last_line'],
+    verified_address['deliverability']
   ].join(',')
 
 end
