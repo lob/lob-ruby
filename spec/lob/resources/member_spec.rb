d209fc41c57e64e072e8f1c0452b4f53fc7c16e3
Sophie Debs <sophie@lob.com>
2019-07-29 17:38:04 -0700

    move group add to beforeeach

--- spec/lob/resources/member_spec.rb
+++ spec/lob/resources/member_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Lob::Resources::Member do
-
+  
   before :each do
     @sample_group_params = {
       description: "Unsubscribe group",
@@ -23,23 +23,21 @@ describe Lob::Resources::Member do
         test: 'stuff'
       }
     }
+    @new_group = subject.groups.create @sample_group_params
   end
 
   subject { Lob::Client.new(api_key: API_KEY) }
 
   describe "list" do
     it "should list members in a group" do
-      new_group = subject.groups.create @sample_group_params
-      
-      assert subject.members.list(new_group["id"])["object"] == "list"
+      assert subject.members.list(@new_group["id"])["object"] == "list"
     end
   end
 
 
   describe "create" do
     it "should create a member" do
-      new_group = subject.groups.create @sample_group_params
-      result = subject.members.create(new_group["id"], @sample_member_params)
+      result = subject.members.create(@new_group["id"], @sample_member_params)
 
       result["description"].must_equal(@sample_member_params[:description])
     end
@@ -48,10 +46,9 @@ describe Lob::Resources::Member do
 
   describe "find" do
     it "should find a member" do
-      new_group = subject.groups.create @sample_group_params
-      new_member = subject.members.create(new_group["id"], @sample_member_params)
+      new_member = subject.members.create(@new_group["id"], @sample_member_params)
       
-      find_result = subject.members.find(new_group["id"], new_member["id"])
+      find_result = subject.members.find(@new_group["id"], new_member["id"])
       find_result["description"].must_equal(@sample_member_params[:description])
     end
   end
@@ -59,10 +56,9 @@ describe Lob::Resources::Member do
 
   describe "destroy" do
     it "should delete a member" do
-      new_group = subject.groups.create @sample_group_params
-      new_member = subject.members.create(new_group["id"], @sample_member_params)
+      new_member = subject.members.create(@new_group["id"], @sample_member_params)
 
-      delete_result = subject.members.destroy(new_group["id"], new_member["id"])
+      delete_result = subject.members.destroy(@new_group["id"], new_member["id"])
       assert_equal(new_member["id"], delete_result["id"])
     end
   end
