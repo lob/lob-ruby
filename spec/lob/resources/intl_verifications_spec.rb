7138363e01fc06c34bd4c1403d264bea6c6515cb
Matt Piercy <mpiercy@gmail.com>
2019-12-16 14:57:00 -0800

    chore(intl-verification): update international verifications test mode fix

--- spec/lob/resources/intl_verifications_spec.rb
+++ spec/lob/resources/intl_verifications_spec.rb
@@ -15,13 +15,10 @@ describe Lob::Resources::IntlVerifications do
   subject { Lob::Client.new(api_key: API_KEY) }
 
   describe "verify" do
-    it "should return a 403 in test mode" do
-      begin
-        subject.intl_verifications.verify @sample_params
-      rescue Lob::InvalidRequestError => e
-        assert e.http_status == 403
-        assert e._response.headers.include?(:content_type)
-      end
+    it "should verify an international address" do
+      result = subject.intl_verifications.verify @sample_params
+
+      result["recipient"].must_equal("TEST KEYS DO NOT VERIFY ADDRESSES")
     end
   end
 
