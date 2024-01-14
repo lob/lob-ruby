ffa1545b3c432af255a5dbdc3f3afbe50042fab6
Alexey Zapparov <alexey@zapparov.com>
2024-01-14 18:34:15 +0100

    fix: Update configuration specs
    `Lob.configure { ... }` was removed at some point, thus specs needs
    update.

--- spec/api_client_spec.rb
+++ spec/api_client_spec.rb
@@ -17,34 +17,34 @@ describe Lob::ApiClient do
     context 'URL stuff' do
       context 'host' do
         it 'removes http from host' do
-          Lob.configure { |c| c.host = 'http://example.com' }
+          Lob::Configuration.default.host = 'http://example.com'
           expect(Lob::Configuration.default.host).to eq('example.com')
         end
 
         it 'removes https from host' do
-          Lob.configure { |c| c.host = 'https://wookiee.com' }
+          Lob::Configuration.default.host = 'https://wookiee.com'
           expect(Lob::ApiClient.default.config.host).to eq('wookiee.com')
         end
 
         it 'removes trailing path from host' do
-          Lob.configure { |c| c.host = 'hobo.com/v4' }
+          Lob::Configuration.default.host = 'hobo.com/v4'
           expect(Lob::Configuration.default.host).to eq('hobo.com')
         end
       end
 
       context 'base_path' do
         it "prepends a slash to base_path" do
-          Lob.configure { |c| c.base_path = 'v4/dog' }
+          Lob::Configuration.default.base_path = 'v4/dog'
           expect(Lob::Configuration.default.base_path).to eq('/v4/dog')
         end
 
         it "doesn't prepend a slash if one is already there" do
-          Lob.configure { |c| c.base_path = '/v4/dog' }
+          Lob::Configuration.default.base_path = '/v4/dog'
           expect(Lob::Configuration.default.base_path).to eq('/v4/dog')
         end
 
         it "ends up as a blank string if nil" do
-          Lob.configure { |c| c.base_path = nil }
+          Lob::Configuration.default.base_path = nil
           expect(Lob::Configuration.default.base_path).to eq('')
         end
       end
